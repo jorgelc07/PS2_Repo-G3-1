581d70fec634908da3cdfb656879c21a744cd195
jbuitragog <j.buitragog@uniandes.edu.co>
2024-04-04 00:20:56 -0500

    Update 00_Modesl.R

--- scripts/Jaime/00_Modesl.R
+++ scripts/Jaime/00_Modesl.R
@@ -18,54 +18,104 @@ gc()
 ############################################################################-
 
 train <- fread("stores/raw/train_personas.csv")
-people<- fread("stores/raw/train_hogares.csv")
+db<- fread("stores/raw/train_hogares.csv")
 
-train <- merge(train,people,
+train <- merge(train,db,
                       by = 'id', 
                       no.dups = TRUE,
                       all = TRUE,
                       suffixes = "")
-
+train$db <- "train"
 test<- fread("stores/raw/test_personas.csv")
-people <- fread("stores/raw/test_hogares.csv")
+db <- fread("stores/raw/test_hogares.csv")
 
-test<- merge(test,people,
+test<- merge(test,db,
          by = 'id', 
          no.dups = TRUE,
          all = TRUE,
          suffixes = "")
 
-rm(people)
+test$db <- "test"
 
+db <- rbind(test,train, fill=TRUE)
 
-model1 <- c(P6545,P6510,P6580,P6585s1,P6585s2,P6585s3,P6585s4,
-           P6590,P6600,P6620,P6630s1,P6630s2,P6630s3,P6630s4,P6630s6,
-           P7422,P7472,
-           P7495,P7500s2,P7500s3,P7505)
+rm(train, test)
 
-x <- test%>%select(P6545)
+x <- c("P6545","P6510",
+            "P6580","P6585s1","P6585s2","P6585s3","P6585s4",
+            "P6590","P6600","P6620","P6630s1","P6630s2","P6630s3","P6630s4","P6630s6",
+            "P7422","P7472",
+            "P7495","P7500s2","P7500s3","P7505")
 
-dummy <- function(db,var){
-  db%>%mutate(new=sum({{var}}))
-  #db <- db%>%mutate(gen={{var}})
+dummy <- function(var){
+  case_when({{var}}==1~1,{{var}}==2 | {{var}}==9~0)
 }
 
-dummy(x,P6545)
+model <- db%>%select("id","Pobre","Npersug","Dominio","Orden","db", x)%>%
+         mutate(across(x,dummy))
 
-table(test$P6545)
-table(test$dummy)
+count <- function(var){
+    sum({{var}}, na.rm = TRUE)
+  }
 
+db <- model%>%summarize(across(x,count), .by=id)
 
-model1 <- Pobre~Npersug+Dominio+
-          P6545+P6510+P6580+P6585s1+P6585s2+P6585s3+P6585s4+
-          P6590+P6600+P6620+P6630s1+P6630s2+P6630s3+P6630s4+P6630s6+
-          P7422+P7472+
-          P7495+P7500s2+P7500s3+P7505
+model <- model%>%select("id","Pobre","Npersug","Dominio","Orden","db")%>%
+         filter(Orden==1)%>%
+         left_join(db)
 
-glm(Pobre~Npersug+Dominio+
-      P6545+P6510+P6580+P6585s1+P6585s2+P6585s3+P6585s4+
-      P6590+P6600+P6620+P6630s1+P6630s2+P6630s3+P6630s4+P6630s6+
-      P7422+P7472,
-    data = train, family = "binomial")
+for (i in 7:length(x)) {
+  model[[i]]<-model[[i]]/model$Npersug 
+}
 
-str(test$P7422,test$P7472)
+db <- model
+db$Dominio <- as.factor(db$Dominio)
+db$Pobre <- factor(db$Pobre,
+                   levels=c("0","1"),
+                   labels=c("No", "Yes"))
+
+############################################################################-
+# 2. Defining models ----
+############################################################################-
+
+model <-formula(paste0("Pobre~","Dominio + ",paste0(x, collapse = " + ")))
+
+############################################################################-
+# 3. Estimating models ----
+############################################################################-
+
+ctrl <-  trainControl(method = "cv",
+                    number = 5,
+                    classProbs = TRUE,
+                    savePredictions = T)
+
+lr <- train(model,
+                data=db%>%filter(db=="train"),
+                method = "glm",
+                family = "binomial",
+                trControl = ctrl)
+            )
+
+,
+                tuneGrid=expand.grid(
+                  alpha = seq(0,1,by=.2),
+                  lambda =10^seq(10, -2, length = 10)
+                )
+                
+)
+
+############################################################################-
+# 4. Exporting data base to kaggle ----
+############################################################################-
+
+predictSample <- test   %>% 
+  mutate(pobre_lab = predict(model1, newdata = test, type = "raw")    ## predicted class labels
+  )  %>% select(id,pobre_lab)
+
+predictSample<- predictSample %>% 
+  mutate(pobre=ifelse(pobre_lab=="Yes",1,0)) %>% 
+  select(id,pobre)
+
+template<-read.csv("data/sample_submission.csv")
+
+write.csv(predictSample,"classification_elasticnet.csv", row.names = FALSE)
